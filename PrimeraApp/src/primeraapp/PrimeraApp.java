bf25b25fa23d321d560fd034c9c4ff500d5f019a
wilmer <u20232216629@usco.edu.co>
2024-02-12 12:53:34 -0500

    primer pagina

---
+++ PrimeraApp/src/primeraapp/PrimeraApp.java
@@ -0,0 +1,17 @@
+
+package primeraapp;
+
+/**
+ *
+ * @author Wilmer Felipe Castellanos
+ */
+public class PrimeraApp {
+    public static void main(String[] args) {
+        System.out.println("hola mundo");
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+   
+}

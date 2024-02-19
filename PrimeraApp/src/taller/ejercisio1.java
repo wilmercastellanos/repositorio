fc0a503dee399e346fbfc6d9df751bac91825911
wilmer <u20232216629@usco.edu.co>
2024-02-19 14:31:11 -0500

    prueba

---
+++ PrimeraApp/src/taller/ejercisio1.java
@@ -0,0 +1,14 @@
+
+package taller;
+
+/**
+ *
+ * @author Wilmer Felipe Castellanos Salazar cpdigo:20232216629
+ */
+public class ejercisio1 {
+
+        public static void main(String[] args) {
+            System.out.println("hola mundo");
+        }
+}
+
